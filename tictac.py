18ed2b45e9016f1f0d26479fbf27fdda25654bf1
Paul <schwendenman.paul@gmail.com>
2011-09-28 20:22:19 -0400

    Added Error Handling Module

--- tictac.py
+++ tictac.py
@@ -404,6 +404,25 @@ def dump(a):
     print "aidata has %i items" % (len(a))
   b.close()
 
+# * * * * * * * * * *
+# * Error Catching  *
+# * * * * * * * * * *
+
+def handleError():
+  import sys
+  stop = 1
+  line = []
+  line.append(sys.exc_info()[2].tb_lineno)
+  tb = sys.exc_info()[2].tb_next
+  while stop:
+    if tb == None:
+      stop = 0
+    else:
+      line.append(tb.tb_lineno)
+      tb = tb.tb_next
+  print "\t", sys.exc_info()[0], sys.exc_info()[1], "\n\t line no: ", line[-1], "\n\t traceback: ", line
+
+
 # * * * * *
 # * Main  *
 # * * * * *
@@ -456,8 +475,7 @@ def main():
   except (ValueError, IndexError, EOFError):
     if DEBUG:
       print "Caught Error: User quit?"
-      import sys
-      print sys.exc_info()[0]
+      handleError()
   printStats(statdata)
   dump(aidata)
 
